823b8800c3bd74b88c9b7514c51d953df1a086b0
jaehee <gjald36@gmail.com>
2023-02-14 12:53:17 +0900

    0214

---
+++ day0214/ex4_random.js
@@ -0,0 +1,29 @@
+// 임의의 숫자를 구한다 Math.random()
+var a = Math.random();
+console.log(a); // 0.0000 ~ 0.9999 사이의 실수를 랜덤으로
+console.log("0~9 사이의 난수 5개 구하기");
+for(let i=1; i<=5;i++){
+    let r=parseInt(Math.random()*10);
+    console.log(r);
+} 
+
+console.log("0~99 사이의 난수 5개 구하기");
+for(let i=1; i<=5;i++){
+    let r=parseInt(Math.random()*100);
+    console.log(r);
+} 
+
+console.log("1~10 사이의 난수 5개 구하기");
+for(let i=1; i<=5;i++){
+    let r=parseInt(Math.random()*10)+1;
+    console.log(r);
+} 
+
+console.log("1~45 사이의 난수 5개 구하기");
+for(let i=1; i<=5;i++){
+    let r=parseInt(Math.random()*45)+1;
+    console.log(r);
+} 
+
+
+
